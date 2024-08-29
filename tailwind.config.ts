ddf512b93a27fbee917c4049bfabb04ff10a0b3f
Swaraj <sharma.swaraj0403@gmail.com>
2024-08-29 15:11:11 +0530

    add custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +7,37 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: {
+          light: "#FFFFFF",
+          "light-secondary": "#F4F7FD",
+          dark: "#2B2C37",
+          "dark-secondary": "#20212C",
+        },
+        button: {
+          primary: "#635FC7",
+          "primary-hover": "#A8A4FF",
+          secodary: "#EA5555",
+          "secondary-hover": "#FF9898",
+          destructive: "#EA5555",
+          "destructive-hover": "#FF9898",
+        },
+        text: {
+          light: "#000112",
+          "light-muted": "#828FA3",
+          dark: "#FFFFFF",
+          "dark-muted": "#828FA3",
+        },
+        fontSize: {
+          "heading-xl": "24px",
+          "heading-l": "18px",
+          "heading-m": "15px",
+          "heading-s": "12px",
+          "body-l": "13px",
+          "body-m": "12px",
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;
