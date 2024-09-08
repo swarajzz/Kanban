27fc9f03759f2c16eb8ec21cf3ef21f0b1ae9f85
Swaraj <sharma.swaraj0403@gmail.com>
2024-09-08 12:51:47 +0530

    styled header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import ellipses from "@/public/icon-vertical-ellipsis.svg"
+import Image from "next/image";
+import { Button } from "./Button";
 
 export default function Header() {
   return (
-    <div className="bold bg-background-dark flex">
-      <h1>Platform Launch</h1>
-      <div>
-        <button>+ Add new task</button>
-        <div>dropdown</div>
+    <div className="bg-primary-400 flex justify-between items-center p-5">
+      <h1 className="text-xl font-bold">Platform Launch</h1>
+      <div className="flex items-center gap-5">
+        <Button size="md" intent={"primary"}>+ Add new task</Button>
+        <Image src={ellipses} alt="ellipses icon" />
       </div>
     </div>
   );
