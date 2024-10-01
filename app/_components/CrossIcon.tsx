af75bfe52e75df1648ecba73a6fa4fa4a751c29a
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-01 12:50:39 +0530

    update form + re-usable crossIcon component

---
+++ app/_components/CrossIcon.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function CrossIcon({
+  width,
+  height,
+  toggleDialog,
+}: {
+  width?: number;
+  height?: number;
+  toggleDialog?: () => void;
+}) {
+  return (
+    <div
+      className="cursor-pointer fill-primary-300 transition delay-75 hover:fill-red-400"
+      onClick={toggleDialog}
+    >
+      <svg
+        width={width}
+        height={height}
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fillRule="evenodd"
+        >
+          <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
+          <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default CrossIcon;
--- app/_components/NewBoard.tsx
+++ app/_components/NewBoard.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { RefObject, useRef, useState } from "react";
-import CrossIcon from "@/public/svgs/icon-cross.svg";
-import Image from "next/image";
+import React, { RefObject } from "react";
 import { Button } from "./Button";
+import CrossIcon from "./CrossIcon";
 
 function NewBoard({
   dialogRef,
@@ -15,7 +14,6 @@ function NewBoard({
     <dialog
       className="relative z-10 transition delay-100 duration-300 ease-in-out"
       aria-labelledby="modal-title"
-      // role="dialog"
       aria-modal="true"
       ref={dialogRef}
     >
@@ -26,21 +24,13 @@ function NewBoard({
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-primary-500 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="flex justify-between px-4 pb-4 pt-5">
+          <div className="relative my-8 w-full max-w-md transform overflow-hidden rounded-lg bg-primary-500 text-left shadow-xl transition-all">
+            <div className="flex items-center justify-between px-6 py-7">
               <h2 className="text-lg text-white">Add New Board</h2>
-              <Image
-                src={CrossIcon}
-                className="cursor-pointer"
-                alt="cross icon"
-                onClick={toggleDialog}
-                style={{ objectFit: "contain", width: "16px", height: "16px" }}
-              />
+              <CrossIcon width={24} height={24} toggleDialog={toggleDialog} />
             </div>
 
-            {/* <div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4"> */}
-            <form className="flex flex-col gap-4 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              {/* <FormRow /> */}
+            <form className="flex flex-col gap-4 px-8 pb-4">
               <div className="flex flex-col">
                 <label className="mb-2 text-sm text-white" htmlFor="boardName">
                   Board Name
@@ -52,26 +42,35 @@ function NewBoard({
                   type="text"
                 />
               </div>
-              <fieldset>
-                {/* <label htmlFor="">Board Columns</label> */}
-                <legend className="mb-2 text-white">Board Columns</legend>
-                <input
-                  placeholder="e.g Todo"
-                  className="border-grey-300 mb-2 w-auto rounded bg-primary-500 px-4 py-2 text-white"
-                  type="text"
-                />
+              
+              <fieldset className="flex flex-col gap-3">
+                <legend className="mb-3 text-white">Board Columns</legend>
+                <div className="flex items-center gap-4">
+                  <input
+                    placeholder="e.g Todo"
+                    className="border-grey-300 w-full max-w-xl rounded bg-primary-500 px-4 py-2 text-white"
+                    type="text"
+                  />
+                  <CrossIcon />
+                </div>
+                <div className="flex items-center gap-4">
+                  <input
+                    placeholder="e.g Todo"
+                    className="border-grey-300 w-full max-w-xl rounded bg-primary-500 px-4 py-2 text-white"
+                    type="text"
+                  />
+                  <CrossIcon />
+                </div>
               </fieldset>
             </form>
-            <div className="flex flex-col gap-5 px-4 py-3 sm:px-6">
+            <div className="flex flex-col gap-5 px-4 mb-6 sm:px-6">
               <Button
-                // className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                 size="md"
                 intent={"secondary"}
               >
                 + Add New Column
               </Button>
               <Button
-                // className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 size="md"
                 intent={"primary"}
               >
