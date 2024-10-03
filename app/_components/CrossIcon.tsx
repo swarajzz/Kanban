afc175b98464e7a1ab06f07d35e08535382c376d
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-03 11:02:59 +0530

    deafult values for width and height in CrossIcon component

--- app/_components/CrossIcon.tsx
+++ app/_components/CrossIcon.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function CrossIcon({
-  width,
-  height,
+  width = 15,
+  height = 15,
   toggleDialog,
 }: {
   width?: number;
@@ -20,9 +20,7 @@ function CrossIcon({
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g
-          fillRule="evenodd"
-        >
+        <g fillRule="evenodd">
           <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
           <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
         </g>
