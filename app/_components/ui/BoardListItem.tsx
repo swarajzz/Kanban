b1b4e94d7b30a135dc1a372c5d0484ba4a6486a1
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-08 10:42:07 +0530

    add links to sidebar board's list

--- app/_components/ui/BoardListItem.tsx
+++ app/_components/ui/BoardListItem.tsx
@@ -2,11 +2,15 @@
 import { BoardProps } from "@/app/_types/types";
 import { IconBoard } from "@/public/sidebar";
 import Image from "next/image";
+import Link from "next/link";
 
 function BoardListItem({ board: { name } }: { board: BoardProps }) {
   return (
-    <li className="flex items-center gap-3">
-      <Image src={IconBoard} alt="Board Icon" /> <span>{name}</span>
+    <li>
+      <Link className="flex items-center gap-3" href={`/${name}`}>
+        <Image src={IconBoard} alt="Board Icon" />
+        <span>{name}</span>
+      </Link>
     </li>
   );
 }
