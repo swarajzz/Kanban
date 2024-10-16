975abffc433e38906aaf2e31b620fd13fb2706e4
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-16 13:07:50 +0530

    update types for ColumnProps, TaskProps & SubTaskProps

--- app/_types/types.ts
+++ app/_types/types.ts
@@ -5,3 +5,30 @@ export type BoardProps = {
   updatedAt: Date;
   userId: string;
 };
+
+export type SubTaskProps = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type TaskProps = {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  columnId: string;
+  userId: string;
+  subTasks: SubTaskProps[];
+};
+
+export type ColumnProps = {
+  id: string;
+  name: string;
+  boardId: string;
+  tasks: TaskProps[];
+};
