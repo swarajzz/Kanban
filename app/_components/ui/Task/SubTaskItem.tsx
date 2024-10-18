bcc63218c1cf63c0990dccdf2562c347e173e94a
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-18 17:34:19 +0530

    fix styling for checkboxes in Task DIalog

--- app/_components/ui/Task/SubTaskItem.tsx
+++ app/_components/ui/Task/SubTaskItem.tsx
@@ -6,27 +6,38 @@ function SubTaskItem({ subTask }: { subTask: SubTaskProps }) {
   return (
     <li className="w-full cursor-pointer rounded border-gray-200 bg-primary-600 transition hover:bg-accent-200">
       <label
-        className="relative flex cursor-pointer items-center gap-3 pl-3"
         htmlFor={subTask.title.replace(/\+/g, "-")}
+        className="inline-flex w-full cursor-pointer items-center"
       >
-        <div>
-          <input
-            id={subTask.title.replace(/\+/g, "-")}
-            type="checkbox"
-            value=""
-            className="peer h-5 w-5 appearance-none rounded border-primary-300 bg-primary-500 text-accent-200 checked:bg-accent-200"
-          />
-          <span className="absolute left-5 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 peer-checked:opacity-100">
-            <Check width={16} height={16} className="text-white" />
-          </span>
-        </div>
-
-        <label
-          htmlFor={subTask.title.replace(/\+/g, "-")}
-          className="w-full cursor-pointer py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
+        <div
+          className="relative flex cursor-pointer items-center rounded-full p-3"
+          data-ripple-dark="true"
         >
+          <input
+            type="checkbox"
+            className="border-blue-gray-200 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all checked:border-primary-300 checked:bg-accent-200"
+            id={subTask.title.replace(/\+/g, "-")}
+          />
+          <div className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3.5 w-3.5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-width="1"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <h3 className="w-full cursor-pointer text-sm font-medium text-gray-900 dark:text-gray-300">
           {subTask.title}
-        </label>
+        </h3>
       </label>
     </li>
   );
