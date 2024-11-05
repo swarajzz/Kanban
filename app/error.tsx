2247c3a163918ec63a6ac46d8990cdb0c64f0026
Swaraj <sharma.swaraj0403@gmail.com>
2024-11-05 11:21:47 +0530

    add error boundaries for error handling

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        className="text-primary-800 inline-block bg-accent-500 px-6 py-3 text-lg"
+        onClick={reset}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
