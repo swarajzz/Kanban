4d027ae0c7b65c1e577e6604ae0fa117481fec4e
Swaraj <sharma.swaraj0403@gmail.com>
2024-11-11 19:09:05 +0530

    feat: handle create new board with server action

--- app/_components/dialog/NewBoard.tsx
+++ app/_components/dialog/NewBoard.tsx
@@ -7,6 +7,8 @@ import DialogPanel from "./DialogPanel";
 import { createBoard } from "@/app/_lib/actions";
 import { defaultColumns } from "@/app/_lib/utils/constants";
 import { getRandomPlaceholderColumn } from "@/app/_lib/utils/helpers";
+import { HashLoader } from "react-spinners";
+import { useFormState } from "react-dom";
 
 function NewBoard({
   userId,
@@ -21,6 +23,7 @@ function NewBoard({
   const [usedColumnPlaceholders, setColumnPlaceholders] = useState<string[]>(
     [],
   );
+  const [state, formAction] = useFormState(createBoard, null);
 
   function handleAdd() {
     const newPlaceholder = getRandomPlaceholderColumn(usedColumnPlaceholders);
@@ -42,15 +45,10 @@ function NewBoard({
     setBoardColumns((prev) => updatedColumns);
   }
 
-  const createBoardWithId = createBoard.bind(null, userId);
-
   return (
     <Dialog ref={dialogRef} toggleDialog={toggleDialog}>
       <DialogPanel title="Add New Board" toggleDialog={toggleDialog}>
-        <form
-          action={createBoardWithId}
-          className="flex flex-col gap-4 px-8 pb-4"
-        >
+        <form action={formAction} className="flex flex-col gap-4 px-8 pb-4">
           <div className="flex flex-col">
             <label className="mb-2 text-sm text-white" htmlFor="boardName">
               Board Name
@@ -74,13 +72,15 @@ function NewBoard({
                   placeholder={`e.g ${column?.placeholder || ""}`}
                   className="border-grey-300 w-full max-w-xl rounded bg-primary-500 px-4 py-2 text-white"
                   type="text"
-                  name={column.placeholder}
+                  name={`column-${index}`}
                   required
                 />
                 <CloseIcon handleRemove={() => handleRemove(column.id)} />
               </div>
             ))}
           </fieldset>
+
+          <input type="hidden" name="userId" value={userId} />
 
           <div className="mb-6 flex flex-col gap-5">
             <Button
@@ -94,6 +94,7 @@ function NewBoard({
             <Button type="submit" size="md" intent={"primary"}>
               Create New Board
             </Button>
+            {/* <HashLoader color="#635FC7" size={30} /> */}
           </div>
         </form>
       </DialogPanel>
--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,25 +1,40 @@
 "use server";
 
+import { Prisma } from "@prisma/client";
 import prisma from "./prisma";
+import { redirect } from "next/navigation";
 
-export async function createBoard(userId: string, formData: FormData) {
+export async function createBoard(formData: FormData) {
   const boardName = formData.get("boardName") as string;
+  const userId = formData.get("userId") as string;
 
   const columns = Array.from(formData)
-    .map(([name]) => name)
-    .filter(
-      (name) => name !== "boardName" && !name.toLowerCase().includes("action"),
-    );
+    .filter(([key, value]) => key.startsWith("column-"))
+    .map(([name, value]) => value as string);
 
-  await prisma.board.create({
-    data: {
-      name: boardName,
-      userId: userId,
-      columns: {
-        create: columns.map((name) => ({
-          name,
-        })),
+  columns.map((name) => console.log("YOLO", name));
+
+  try {
+    await prisma.board.create({
+      data: {
+        name: boardName,
+        userId: userId,
+        columns: {
+          create: columns.map((columnName) => ({
+            name: columnName,
+          })),
+        },
       },
-    },
-  });
+    });
+    redirect(`/${boardName.trim().replace(/\s+/g, "-").toLowerCase()}`);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        throw new Error(
+          "A board with this name already exists. Please choose a different name.",
+        );
+      }
+    }
+    throw e;
+  }
 }
