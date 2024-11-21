791633a63624763c6709d9c81d36012ed63d2efe
Swaraj <sharma.swaraj0403@gmail.com>
2024-11-21 16:52:00 +0530

    update taskDialog with form custom components & RHF + add Checked Subtask List

--- app/_components/dialog/TaskDialog.tsx
+++ app/_components/dialog/TaskDialog.tsx
@@ -4,25 +4,58 @@ import { Button } from "../ui/Button";
 import DialogPanel from "./DialogPanel";
 import Dialog from "./Dialog";
 import { TaskProps } from "@/app/_types/types";
-import SubTaskItem from "../ui/Task/SubTaskItem";
 import TaskDropdown from "./TaskDropdown";
+import Form from "../ui/Form/Form";
+import FieldSet from "../ui/Form/FieldSet";
+import FormRow from "../ui/Form/FormRow";
+import Input from "../ui/Form/Input";
+import { useFieldArray, useForm } from "react-hook-form";
+import CheckedSubtaskList from "../ui/Task/CheckedSubtaskList";
+import { getCompletedSubtasksLength } from "@/app/_lib/utils/helpers";
 
 function TaskDialog({
   dialogRef,
-  toggleDialog,
   task,
   isShowDropdown,
+  toggleDialog,
   toggleShowDropdown,
   toggleEditDialog,
 }: {
   dialogRef: RefObject<HTMLDialogElement>;
-  toggleDialog: () => void;
   task: TaskProps;
   isShowDropdown: boolean;
+  toggleDialog: () => void;
   toggleShowDropdown: () => void;
   toggleEditDialog: () => void;
 }) {
   const { title, description, subTasks } = task;
+  const completedSubtasks = getCompletedSubtasksLength(subTasks);
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      checked_status: task.status ?? "",
+      checkSubtasks: task.subTasks.map(({ id, isCompleted, title }) => ({
+        id: id,
+        isCompleted: isCompleted,
+        title: title,
+      })),
+    },
+  });
+
+  const { fields } = useFieldArray({
+    name: "checkSubtasks",
+    control,
+  });
+
+  const processForm = async (data) => {
+    console.log(data);
+  };
 
   return (
     <Dialog ref={dialogRef} toggleDialog={toggleDialog}>
@@ -32,55 +65,42 @@ function TaskDialog({
         toggleDialog={toggleDialog}
         toggleShowDropdown={toggleShowDropdown}
       >
-        <form className="txt-xs flex flex-col gap-4 px-8 pb-4">
+        <Form submitHandler={handleSubmit(processForm)}>
           <div className="border-grey-300 rounded bg-primary-500 text-primary-300">
             {description}
           </div>
 
-          <fieldset className="flex flex-col gap-3">
-            <legend className="mb-3 text-white">
-              Subtasks ({subTasks.filter((item) => item.isCompleted).length} of
-              {subTasks.length})
-            </legend>
+          <FieldSet
+            legend={`Subtasks ${completedSubtasks} of
+              ${subTasks.length}`}
+          >
+            <CheckedSubtaskList fields={fields} register={register} />
+          </FieldSet>
 
-            <ul className="flex w-full flex-col gap-3">
-              {subTasks.map((subTask) => (
-                <SubTaskItem key={subTask.id} subTask={subTask} />
-              ))}
-            </ul>
-          </fieldset>
+          <FormRow label="Status" error={errors?.checked_status?.message}>
+            <Input
+              element="select"
+              register={register}
+              validationSchema={{
+                required: "This field is required",
+              }}
+              name="checked_status"
+            >
+              <option disabled value="">
+                -- select an option --
+              </option>
 
-          <div className="flex flex-col">
-            <label className="mb-2 text-sm text-white" htmlFor="boardName">
-              Status
-            </label>
+              <option value="Todo">Todo</option>
+              <option value="Done">Done</option>
+            </Input>
+          </FormRow>
 
-            <div className="relative">
-              <select
-                name="status"
-                id="status-select"
-                className="border-grey-300 relative w-full max-w-xl rounded bg-primary-500 px-4 py-2 text-white"
-              >
-                <option value="Todo">Todo</option>
-                <option value="Done">Done</option>
-              </select>
-
-              <svg
-                width="10"
-                height="7"
-                className="absolute right-2 top-1/2 -translate-y-1/2"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke="#635FC7"
-                  strokeWidth="2"
-                  fill="none"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </div>
+          <div className="mb-6 flex flex-col gap-5">
+            <Button type="submit" size="md" intent={"primary"}>
+              Save Task Changes
+            </Button>
           </div>
-        </form>
+        </Form>
 
         {isShowDropdown ? (
           <TaskDropdown
@@ -91,12 +111,6 @@ function TaskDialog({
         ) : (
           ""
         )}
-
-        <div className="mb-6 flex flex-col gap-5 px-4 sm:px-6">
-          <Button size="md" intent={"primary"}>
-            Save Task Changes
-          </Button>
-        </div>
       </DialogPanel>
     </Dialog>
   );
---
+++ app/_components/ui/Task/CheckedSubtaskList.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { UseFormRegister } from "react-hook-form";
+import Input from "../Form/Input";
+import FormRow from "../Form/FormRow";
+
+function CheckedSubtaskList({ fields, register }) {
+  return (
+    <ul className="flex w-full flex-col gap-3">
+      {fields.map((field, index) => (
+        <li
+          key={field.id}
+          className="w-full cursor-pointer rounded border-gray-200 bg-primary-600 transition hover:bg-accent-200 hover:bg-opacity-25"
+        >
+          <label className="inline-flex w-full cursor-pointer items-center">
+            <div
+              className="peer relative flex cursor-pointer items-center rounded-full p-3"
+              data-ripple-dark="true"
+            >
+              <Input
+                type="checkbox"
+                register={register}
+                name={`checkSubtasks.${index}.isCompleted`}
+              />
+              <div className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-3.5 w-3.5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <h3 className="w-full cursor-pointer text-sm font-medium text-gray-900 peer-has-[:checked]:line-through dark:text-gray-300">
+              {field.title}
+            </h3>
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CheckedSubtaskList;
