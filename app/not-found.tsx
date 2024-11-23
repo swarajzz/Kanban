0487156d8b2715a1a30ee52ff1b29154ca6ed7ea
Swaraj <sharma.swaraj0403@gmail.com>
2024-11-23 12:40:10 +0530

    handle not found error

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+function NotFound() {
+  return (
+    <main className="flex size-full flex-col items-center justify-center gap-6">
+      <h1 className="text-2xl font-semibold">
+        This page could not be found :(
+      </h1>
+      <Link
+        href="/"
+        className="text-primary-800 inline-block bg-accent-500 px-6 py-3 text-lg"
+      >
+        Go back home
+      </Link>
+    </main>
+  );
+}
+
+export default NotFound;
