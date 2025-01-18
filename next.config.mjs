03e8ef988e1d5cfbb1fd21b23aca993024299af0
Swaraj <sharma.swaraj0403@gmail.com>
2025-01-18 13:21:31 +0530

    package and config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack", "url-loader"],
+  //   });
+
+  //   return config;
+  // },
+};
 
 export default nextConfig;
