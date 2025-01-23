9f5cddb30b096644e310660f93b9c48809e30b0e
Swaraj <sharma.swaraj0403@gmail.com>
2025-01-23 19:50:22 +0530

    fix vercel deployment issue

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,3 @@ export const middleware = auth;
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-
-export const runtime = "experimental-edge";
